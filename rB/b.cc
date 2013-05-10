00341ef3bad6bca107d22a8bf99f0ca8c9512b07
krime <krimelam@gmail.com>
2013-05-10 22:43:35 +0800

    Problem B

--- rB/b.cc
+++ rB/b.cc
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+#define N 1000000
+
 int main(void) {
+  int t,tt,n,x,y;
+  cin>>t;
+  tt=t;
+  while (t--) {
+    cin>>n>>x>>y;
+    cout<<"Case #"<<tt-t<<": ";
+    
+  }
   return 0;
 }
