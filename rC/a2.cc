c2a743bec57923549e49555caa66a2758df89203
krime <krimelam@gmail.com>
2013-05-15 14:01:10 +0800

    Update Problem A

--- rC/a2.cc
+++ rC/a2.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <sstream>
-
+#include <string>
 #include <iterator>
-
+#include <algorithm>
 #include <cstring>
 
 #define L 1000001
-char name[L];
 
 using namespace std;
+string name;
 
 int main(void) {
   /*
@@ -32,18 +32,23 @@ int main(void) {
   cin>>t;
   tt=t;
   while (t--) {
-    cout<<"Case #"<<tt-t<<": ";
+    name.clear();
     c=0;
-    memset(name,0,sizeof(name)*sizeof(char));
-    cin>>noskipws;
-
-    istream_iterator<char> isr=istream_iterator<char>(cin);
-    istream_iterator<char> eof;
-    
-    for (int i=0;*isr!=' ';i++) {
-      name[i]=*isr++;
-      cout<<name[i];
+    cout<<"Case #"<<tt-t<<": ";
+    cin>>name>>n;
+    l=name.size();
+    string v=string(n,'1');
+    for (int i=0;i<l;i++)
+      name[i]=alpha[name[i]-'a']+'0';
+    for (int i=0;i<l;i++) {
+      for (int j=i+n;j<=l;j++) {
+        if (search(name.begin()+i,name.begin()+j,v.begin(),v.end())!=name.begin()+j) {
+          c++;
+        }
+      }
     }
+    //cout<<name<<' '<<n<<v<<endl;
+    cout<<c<<endl;
   }
   return 0;
 }
