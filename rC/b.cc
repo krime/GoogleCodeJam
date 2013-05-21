0962ed9a38dd8813c26feda6197c4de4095ca673
krime <krimelam@gmail.com>
2013-05-21 22:25:45 +0800

    Update Problem B

--- rC/b.cc
+++ rC/b.cc
@@ -1,9 +1,57 @@
-#include <iostream>
+#include<algorithm>
+#include<cassert>
+#include<complex>
+#include<map>
+#include<iomanip>
+#include<sstream>
+#include<queue>
+#include<set>
+#include<string>
+#include<vector>
+#include<iostream>
+#include<cstring>
+#include<cmath>
 
+#pragma comment(linker, "/stack:64000000")
+
+#define FOR(i, a, b) for(int i =(a); i <=(b); ++i)
+#define FORD(i, a, b) for(int i = (a); i >= (b); --i)
+#define fup FOR
+#define fdo FORD
+#define REP(i, n) for(int i = 0;i <(n); ++i)
+#define VAR(v, i) __typeof(i) v=(i)
+#define FORE(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define siz SZ
+#define CLR(x) memset((x), 0, sizeof(x))
+#define PB push_back
+#define MP make_pair
+#define X first
+#define Y second 
+#define FI X
+#define SE Y
+#define SQR(a) ((a)*(a))
+#define DEBUG 1
+#define debug(x) {if (DEBUG)cerr <<#x <<" = " <<x <<endl; }
+#define debugv(x) {if (DEBUG) {cerr <<#x <<" = "; FORE(it, (x)) cerr <<*it <<", "; cout <<endl; }}
 using namespace std;
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int>P;
+typedef vector<int>VI;
+const int INF=1E9+7;
+LD DIS(LD x, LD y) {return sqrt(x*x+y*y);}
+const LD PI = acos(LD(0.0))*2;
+LL GCD(LL a, LL b){return b ? GCD(b,a%b) : a;}
+
+template<class C> inline void mini(C&a4, C b4){a4=min(a4, b4); }
+template<class C> inline void maxi(C&a4, C b4){a4=max(a4, b4); }
+template<typename T> inline T abs(T a){ return ((a < 0) ? -a : a); }
+template<typename T> inline T sqr(T a){ return a * a; }
+
+#define MAXN 100007
 
 int main(void) {
-  int a=1E9;
-  cout<<a<<endl;
   return 0;
 }
